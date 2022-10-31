e334568bb430189cf1432e7c1ba73b37fb1aea85
arrafi-musabbir <musabbir.arrafi@gmail.com>
2022-10-31 14:20:43 +0600

    beta v1

--- script.py
+++ script.py
@@ -4,11 +4,11 @@ import time
 
 browser= webdriver.Chrome(ChromeDriverManager().install())
 
-browser.get('https://www.youtube.com')
+browser.get('https://www.google.com')
 i=0 
 while i<5:
     #Refreshes the web page 5times with 30 seconds interval
     browser.refresh()
     print("refreshed: {}".format(i))
-    time.sleep(30)
+    time.sleep(1)
     i+=1
