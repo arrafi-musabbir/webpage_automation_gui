2a2bde4fa9f99b8661619e96b2686213ff0a12e2
arrafi-musabbir <musabbir.arrafi@gmail.com>
2022-11-26 08:42:37 +0600

    script for login

--- script.py
+++ script.py
@@ -1,14 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as BraveService
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+from webdriver_manager.core.utils import ChromeType
+from random import randint
+from time import sleep
+from selenium.webdriver.common.by import By
 
-browser= webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=BraveService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
+# Time to wait for element's presence
+driver.implicitly_wait(10)
+driver.get('https://www.instagram.com/')
 
-browser.get('https://www.google.com')
-i=0 
-while i<5:
-    #Refreshes the web page 5times with 30 seconds interval
-    browser.refresh()
-    print("refreshed: {}".format(i))
-    time.sleep(1)
-    i+=1
+# Sleep a random number of seconds (between 5 and 10)
+sleep(randint(5,10))
+
+# # Click 'Accept cookies' button on www.instagram.com
+# accept_cookis_button = driver.find_element(By.XPATH, '//button[text()="Only allow essential cookies"]')
+# accept_cookis_button.click()
+
+# sleep(randint(5,10))
+
+username_input = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
+password_input = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+username_input.send_keys("yourUsername")
+password_input.send_keys("yourP@ssw0rd")
+
+sleep(randint(5,10))
+
+login_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
+login_button.click()
+
+# Close the browser after 100 seconds
+sleep(5)
+driver.close()
